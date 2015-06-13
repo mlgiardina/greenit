8265a395412162ace7708cf721a7d1196d5d3f6d
Matthew Giardina <mlgiardina@me.com>
2015-06-13 12:55:28 -0500

    can now add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to :back
+    else
+      flash[:alert] = "An Error Occurred."
+      render :back
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :link_id)
+  end
+
 end
