15f60cec9bde9ef898f2be54e6a05257026d972f
Matthew Giardina <mlgiardina@me.com>
2015-06-13 15:31:23 -0500

    created a show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
