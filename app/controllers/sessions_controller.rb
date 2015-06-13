14532d72efd876ecd714bf9c907e87538574d4a4
Matthew Giardina <mlgiardina@me.com>
2015-06-13 11:36:00 -0500

    can now create new users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     user = User.find_by_name(params[:name])
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to root_path, notice: "Account created!"
+    else
+      flash[:alert] = "Errors Occurred."
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
