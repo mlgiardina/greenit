887ce4ea4d7563261338083c8ceb0475a4a4bd15
Matthew Giardina <mlgiardina@me.com>
2015-06-17 22:49:48 -0500

    slight change to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ get 'login', to: 'sessions#new', as: 'login'
 post 'login', to: 'sessions#create', as: 'create_session'
 get 'logout', to: 'sessions#destroy', as: 'logout'
 
- root 'links#index'
+root 'links#index'
 end
